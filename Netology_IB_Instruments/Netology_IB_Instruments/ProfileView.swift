8e2957855f613b1600c8a1a847daf6a310eb7614
Sergey <moonangeal@mail.ru>
2022-04-13 19:09:21 +0300

    Add task iosui-1

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Сергей  Быков on 11.04.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var town: UILabel!
+    @IBOutlet weak var imageUser: UIImageView!
+    @IBOutlet weak var userText: UITextView!
+    
+
+}
